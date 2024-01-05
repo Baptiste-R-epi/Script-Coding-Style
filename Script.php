7140b0323ab458dbbd0c33d82b78ecd36781dd6f
Baptiste-R-epi <baptiste.royer@epitech.eu>
2024-01-05 18:04:06 +0100

    Push

---
+++ Script.php
@@ -0,0 +1,45 @@
+<?php
+
+const IGNORED_FOLDER = [
+	"truc",
+];
+
+
+const REPLACE = [
+	"/(\w+ \**\w+\([\w\s,*]+\))\s*{/" => "$1\n{",						// put function braces at a new line
+	"@(\t+.*?)//.*@" => "",												// delete coms:// at end of line starting with tabs
+	"/[^\S\n]*(for|if|while)\s*(\([^{}]*\))\s*{\s*}[^\S\n]*\n/" => "",	// delete empty for/if/while
+	"/(for|if|while)\s*(\([^{}]*\))\s*{/" => "$1 $2 {",					// put for/if/while braces at the end of line
+	"/}\s*else\s*({?)/" => "} else $1",									// manage else braces including else if : } else {
+];
+
+
+function get_all_files($path) {
+	global $argv;
+	$files = scandir($path);
+	$output = [];
+	foreach ($files as $file_path) {
+		if (
+			$file_path[0] == "." ||
+			in_array($path . "/" . $file_path, IGNORED_FOLDER)
+		)
+			continue;
+		if (is_dir($path . "/" . $file_path)) {
+			array_push($output, ...get_all_files($path . "/" . $file_path));
+		} elseif (substr($file_path, -2) == ".c") {
+			array_push($output, $path . "/" . $file_path);
+		}
+	}
+	return $output;
+}
+
+$all_files = get_all_files(".");
+
+
+foreach ($all_files as $file_path) {
+	$content = file_get_contents($file_path);
+	foreach (REPLACE as $regex => $replace) {
+		$content = preg_replace($regex, $replace, $content);
+	}
+	file_put_contents($file_path, $content);
+}
